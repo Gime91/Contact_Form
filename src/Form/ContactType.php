16bee3e69c4a7e498c7b24c1d3eadd1ef47a9bee
Gimesi Ádám <gadaam22@gmail.com>
2023-06-19 11:41:30 +0200

    project to github

---
+++ src/Form/ContactType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Név',
+            ])
+            ->add('email', null, [
+                'label' => 'Email',
+            ])
+            ->add('message', null, [
+                'label' => 'Üzenet',
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
